2f201beaa511b096ac829f9922beacdf55d4e141
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-07-15 11:48:44 +0530

    Day 6

--- Day 6/script.js
+++ Day 6/script.js
@@ -2,11 +2,16 @@ const quoteBtn = document.getElementById("quoteBtn");
 const quoteDisplay = document.getElementById("quote");
 
 const quotes = [
-  "Believe in yourself!",
-  "Code like there's no bug!",
-  "Push yourself, because no one else is going to do it.",
-  "Don't watch the clock; do what it does. Keep going.",
-  "Great things never come from comfort zones."
+  "Dream big, code bigger.",
+"Khud par bharosa rakho, compiler bhi galtiyan maanta hai.", 
+"Every error is a step closer to perfection.",
+"Seekh raha hai? Toh jeet raha hai!" ,
+"Success doesn’t come overnight. It’s coded line by line.",
+"Focus kro output pe, warnings apne aap hat jayengi." ,
+"Write code that your future self will thank you for.",
+"Koi shortcut nahi hota, bas consistent rehna padta hai.",
+"From 'Hello World' to 'Hello Career' – keep going!",
+"Failure is not fatal, infinite loops are."
 ];
 
 const colors = ["#f8c291", "#6a89cc", "#82ccdd", "#b8e994", "#f6b93b", "#e55039"];
